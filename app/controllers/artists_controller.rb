0eb5eb42359b0d923c09bb147a13d392295b8980
Rui Freitas <dinnerpartysaboteur@gmail.com>
2020-04-15 22:57:18 +0100

    Rails new and Artist model with index and show actions

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,11 @@
+# ArtistsController.new
+
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+end
